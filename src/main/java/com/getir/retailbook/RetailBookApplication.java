a728ce346b6aec226a5cdc3b9729274da60ad6df
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-03 22:42:51 +0300

    add: Initial commit

---
+++ src/main/java/com/getir/retailbook/RetailBookApplication.java
@@ -0,0 +1,13 @@
+package com.getir.retailbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getir/retailbook/RetailBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getir.retailbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
