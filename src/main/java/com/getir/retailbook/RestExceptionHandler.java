6442093234cd8640fc09ef8d54045faa25d9a1f9
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-06 22:49:32 +0300

    add: RestExceptionHandler added

---
+++ src/main/java/com/getir/retailbook/RestExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.getir.retailbook;
+
+import com.getir.retailbook.util.MessageConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(value = Exception.class)
+    public ErrorResponse defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        logger.error(e.getMessage(), e);
+        return new ErrorResponse(MessageConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = { IllegalArgumentException.class })
+    protected ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex, WebRequest request) {
+
+        return new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = { MethodArgumentNotValidException.class })
+    protected ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+
+        String validationErrors = "";
+        for(ObjectError o : ex.getBindingResult().getAllErrors())
+            validationErrors += o.getDefaultMessage();
+
+        return new ErrorResponse(validationErrors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = { MissingServletRequestParameterException.class })
+    protected ErrorResponse handleMissingServletRequestParameterException(MissingServletRequestParameterException ex, WebRequest request) {
+
+        return new ErrorResponse(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = { NoSuchElementException.class })
+    protected ErrorResponse handleNoSuchElementException(NoSuchElementException ex, WebRequest request) {
+        return new ErrorResponse(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/getir/retailbook/util/MessageConstant.java
@@ -0,0 +1,5 @@
+package com.getir.retailbook.util;
+
+public class MessageConstant {
+    public static final String SOMETHING_WENT_WRONG = "Something went wrong.    ";
+}
