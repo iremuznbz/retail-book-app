1d3d85d2c42a61d00aab95af1bf699a3e2551914
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-08 00:51:18 +0300

    add: password encoder and jwt secret

--- src/main/java/com/getir/retailbook/RetailBookApplication.java
+++ src/main/java/com/getir/retailbook/RetailBookApplication.java
@@ -2,11 +2,18 @@ package com.getir.retailbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class RetailBookApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailBookApplication.class, args);
