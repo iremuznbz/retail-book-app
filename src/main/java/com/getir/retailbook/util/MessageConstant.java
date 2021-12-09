4a643dd3bdc9c2ddb4e56c3ffb10c8f1335145c3
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-09 23:28:56 +0300

    add: Exception message constant

--- src/main/java/com/getir/retailbook/util/MessageConstant.java
+++ src/main/java/com/getir/retailbook/util/MessageConstant.java
@@ -2,4 +2,6 @@ package com.getir.retailbook.util;
 
 public class MessageConstant {
     public static final String SOMETHING_WENT_WRONG = "Something went wrong.    ";
+    public static final String CANNOT_CREATE_SAME_ENTITY = "You cannot create same object";
+    public static final String SOMETHING_WENT_WRONG_WITH_MONGO = "Something went wrong with mongo";
 }
