bfed01e901a963f4f027af9771aae24a5284d802
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-11 10:44:32 +0300

    add: application properties test profile

--- src/test/java/com/getir/retailbook/RetailBookApplicationTests.java
+++ src/test/java/com/getir/retailbook/RetailBookApplicationTests.java
@@ -2,8 +2,10 @@ package com.getir.retailbook;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RetailBookApplicationTests {
 
 	@Test
