ca740bf93e7702c3a15bbf2440f86975a86d6ade
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-11 18:15:14 +0300

    add: createdby and lastmodified added BaseEntity

--- src/main/java/com/getir/retailbook/BaseEntity.java
+++ src/main/java/com/getir/retailbook/BaseEntity.java
@@ -1,6 +1,8 @@
 package com.getir.retailbook;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 
 import java.time.LocalDateTime;
 
@@ -9,11 +11,33 @@ public class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
 
+    @LastModifiedBy
+    private LocalDateTime lastModifiedBy;
+
+    @CreatedBy
+    private String createdBy;
+
     public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
     public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(LocalDateTime lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
     }
 }
