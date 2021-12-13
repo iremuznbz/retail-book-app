79eb4def0c64330375c7ae5395385c0d1952f724
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-14 00:05:43 +0300

    fix: Order statistics aggregation

--- src/main/java/com/getir/retailbook/order/repo/OrderRepository.java
+++ src/main/java/com/getir/retailbook/order/repo/OrderRepository.java
@@ -2,39 +2,43 @@ package com.getir.retailbook.order.repo;
 
 import com.getir.retailbook.order.OrderEntity;
 import com.getir.retailbook.statistics.OrderStatisticDto;
-import org.bson.Document;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 public interface OrderRepository extends MongoRepository<OrderEntity, String> {
     List<OrderEntity> findByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 
     List<OrderEntity> findOrderEntitiesByCustomerId(String customerId);
 
-    @Aggregation( pipeline = {
-            "    $match" +
-            "    { customerId: '?0' } " +
-            "    $unwind" +
-            "    { path: '$items' }," +
-            "    $group" +
-            "    {" +
-            "        _id:  { month: {$month: '$createdAt'}}," +
-            "        totalAmount: { $sum:  { $toDecimal: '$items.totalAmount'}}," +
-            "        totalOrderCount: {$sum : 1}," +
-            "        totalBookCount: {$sum: '$items.quantity'}" +
-            "    }," +
-            "    $project" +
-            "    {" +
-            "            totalAmount: '$totalAmount'," +
-            "            totalOrderCount: '$totalOrderCount'," +
-            "            totalBookCount: '$totalBookCount'," +
-            "            month: '$_id'," +
-            "    }"})
+    @Aggregation( pipeline = {"{$match: { customerId: '?0' }}", "{$unwind: { path: '$items'}}", "{\n" +
+            "    $group: {\n" +
+            "        _id: {\n" +
+            "            $month: '$createdAt'\n" +
+            "        },\n" +
+            "        totalAmount: {\n" +
+            "            $sum: {\n" +
+            "                $toDecimal: '$items.totalAmount'\n" +
+            "            }\n" +
+            "        },\n" +
+            "        totalOrderCount: {\n" +
+            "            $sum: 1\n" +
+            "        },\n" +
+            "        totalBookCount: {\n" +
+            "            $sum: '$items.quantity'\n" +
+            "        }\n" +
+            "    }\n" +
+            "}, {\n" +
+            "    $project: {\n" +
+            "        totalAmount: '$totalAmount',\n" +
+            "        totalOrderCount: '$totalOrderCount',\n" +
+            "        totalBookCount: '$totalBookCount',\n" +
+            "        month: '$_id'\n" +
+            "    }\n" +
+            "}"})
     AggregationResults<OrderStatisticDto> getCustomerStatisticsById(String customerId);
 
 }
