b912b478117771433c4f11dff537053310e1b984
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-15 00:09:11 +0300

    add: MongoConfig

---
+++ src/main/java/com/getir/retailbook/config/MongoConfig.java
@@ -0,0 +1,8 @@
+package com.getir.retailbook.config;
+
+public class MongoConfig {
+    /*    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }*/
+}
