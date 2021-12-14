cb0363330017b924cd25a0d71e1d3d06aebc0869
Irem Uzunbaz <irem.uzunbaz@sadeyazilim.com>
2021-12-15 00:06:01 +0300

    ref: datamongotest congig

--- src/test/java/com/getir/retailbook/RetailBookApplicationTests.java
+++ src/test/java/com/getir/retailbook/RetailBookApplicationTests.java
@@ -1,11 +1,11 @@
 package com.getir.retailbook;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
+@DataMongoTest
 class RetailBookApplicationTests {
 
 	@Test
